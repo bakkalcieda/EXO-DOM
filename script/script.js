8c427e5eba5c7f17e2e9af7ff7003f24ed0e9edc
BAKKALCI Eda <stg482043@stg-bf.be>
2026-05-19 15:41:56 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+/*Créer un nouveau projet Web :
+
+un fichier html
+un fichier js
+un fichier de style (css, sass, scss, ...)
+Créer un gestionnaire de contacts. Permettre à un utilisateur d'encoder le nom, le prénom, l'email et le numéro de téléphone.
+
+Pour chaque ajout, on ajoute une DIV à la page présentant au format carte les infos du contact.
+pas de tableau 
+Attention le contact ne doit être ajoutée qu'une fois */
