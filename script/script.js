a2183d6b02981844f5e26d41f9b203283678de57
BAKKALCI Eda <stg482043@stg-bf.be>
2026-05-21 13:15:44 +0200

    Exo Final

--- script/script.js
+++ script/script.js
@@ -9,6 +9,7 @@ Pour chaque ajout, on ajoute une DIV à la page présentant au format carte les 
 pas de tableau 
 Attention le contact ne doit être ajoutée qu'une fois */
 const contacts = {};
+
 const form         = document.getElementById("contactForm");
 const inputNom     = document.getElementById("nom");
 const inputPrenom  = document.getElementById("prenom");
@@ -18,19 +19,66 @@ const errorMessage = document.getElementById("errorMessage");
 const contactsGrid = document.getElementById("contactsGrid");
 const emptyMessage = document.getElementById("emptyMessage");
 const contactCount = document.getElementById("contactCount");
+let compteur = 0;
+
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    hideError();
-const nom       = capitalizeFirst(inputNom.value.trim());
-const prenom    = capitalizeFirst(inputPrenom.value.trim());
-const email     = inputEmail.value.trim().toLowerCase();
-const telephone = inputTel.value.trim();
+    
+    const nom       = inputNom.value.trim();
+    const prenom    = inputPrenom.value.trim();
+    const email     = inputEmail.value.trim().toLowerCase();
+    const telephone = inputTel.value.trim();
+    
     if (!nom || !prenom || !email || !telephone) {
-        showError("!!Veuillez remplir tous les champs!!");
+        errorMessage.textContent = "Veuillez remplir tous les champs!!";
+        errorMessage.classList.remove("hidden");
         return;
     }
-    if (!isValidEmail(email)) {
-        showError("!! L'adresse email n'est pas valide !!");
-        inputEmail.classList.add("input-error");
-        return;
-    }
+    
+    const doublon = Object.values(contacts).some(
+        (c) => c.email === email || c.telephone === telephone || c.nom === nom || c.prenom === prenom
+        );
+        
+        if (doublon) {
+            errorMessage.textContent = "Ce contact existe dejja!!";
+            errorMessage.classList.remove("hidden");
+            return;
+        }
+        // je peux utiliser aussi compteur avec date.now//
+        //const id = Date.now(); //
+        //calcule en miliseconde pour un id unique  //
+        const id = compteur++;
+        contacts[id] = { id, nom, prenom, email, telephone };
+        
+        const carte = document.createElement("div");
+        carte.classList.add("carte");
+        carte.setAttribute("data-id", id);
+        carte.innerHTML = `
+        <div class="avatar">${prenom[0]}${nom[0]}</div>
+        <h3>${prenom} ${nom}</h3>
+        <p>${email}</p>
+        <p>${telephone}</p>
+        <button onclick="supprimerContact(${id})">Supprimer</button>
+        `;
+        
+        contactsGrid.appendChild(carte);
+        emptyMessage.style.display = "none";
+        contactCount.textContent = `(${Object.keys(contacts).length})`;
+        
+        errorMessage.textContent = "";
+        errorMessage.classList.add("hidden");
+        form.reset();
+    });
+    
+    // suppresiion contact //
+    
+    function supprimerContact (id) {
+        delete contacts [id];
+        document.querySelector(`[data-id = "${id}"]`).remove();
+        contactCount.textContent = `(${Object.keys(contacts).length})`;
+        
+        if (Object.keys(contacts).length === 0){
+            emptyMessage.style.display = "block";
+        }
+    } 
